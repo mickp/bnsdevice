7a411210fb82eb38089fedd2f99869c9c7ee52bb
map <mick.phillips@gmail.com>
2014-05-16 10:24:15 -0600

    Changes to bnstest objects.

--- bnstest.py
+++ bnstest.py
@@ -2,6 +2,9 @@ arrayA = range(256)
 arrayB = range(256)
 list.reverse(arrayB)
 LINE0 = sum(map(list, zip(arrayA,arrayB)),[])
+hi = 65535
+lo = 0
+
 INTERLACED = []
 
 LUTPATH = "C:/BLINK_PCIe/LUT_Files/"
@@ -13,12 +16,21 @@ for n in range(512):
 BLACK = []
 WHITE = []
 for n in range(512 * 512):
-	BLACK.extend([0])
-	WHITE.extend([16383])
+	BLACK.extend([lo])
+	WHITE.extend([hi])
 
-STRIPES=[]
+VSTRIPES=[]
 for n in range(512):
-	hi = 16384
-	STRIPES.extend((512/8) * [0,0,hi,hi,0,0,hi,hi])
+	VSTRIPES.extend((512/8) * [lo,lo,hi,hi,lo,lo,hi,hi])
 	
-SERIES = [INTERLACED,BLACK,STRIPES,WHITE]
+HSTRIPES=[]
+for n in range(512/4):
+	HSTRIPES.extend(2*512*[lo] + 2*512*[hi])
+	
+DSTRIPES = []
+for n in range((512/6)-1):
+	DSTRIPES.extend(3*[lo]+3*[hi])
+DSTRIPES.extend((512*512 - len(DSTRIPES)) * [lo])
+
+	
+SEQ = [BLACK,WHITE,HSTRIPES,DSTRIPES,VSTRIPES]
