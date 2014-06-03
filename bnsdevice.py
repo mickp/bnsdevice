8ea75018819b4402a1f295453a8638e4d8501314
map <mick.phillips@gmail.com>
2014-06-03 09:34:42 +0100

    Added copyright and licensing information (GPL).

--- bnsdevice.py
+++ bnsdevice.py
@@ -1,38 +1,56 @@
+# Copyright Mick Phillips, 2014 <mick.phillips at gmail.com>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import ctypes
 from ctypes import c_int, c_bool, c_double, c_short, c_char, c_char_p, c_uint, c_ushort, windll
 
 CLASS_NAME = "BNSDevice"
 
 class BNSDevice(object):
-    # === BNS Interface.dll functions ===
-    # + indicates equivalent python implementation here
-    # o indicates calls from non-equivalent python code here
-    #
-    # ==== Documented ====
-    # + int Constructor (int LCType={0:FLC;1:Nematic})
-    # + void Deconstructor ()
-    # + void ReadTIFF (const char* FilePath, unsigned short* ImageData, unsigned int ScaleWidth, unsigned int ScaleHeight) 
-    # + void WriteImage (int Board, unsigned short* Image)
-    # + void LoadLUTFile (int Board, char* LUTFileName)
-    # o void LoadSequence (int Board, unsigned short* Image, int NumberOfImages)
-    # + void SetSequencingRate (double FrameRate)
-    # + void StartSequence ()
-    # + void StopSequence ()
-    # + bool GetSLMPower (int Board)
-    # + void SLMPower (int Board, bool PowerOn)
-    # + void WriteCal (int Board, CAL_TYPE Caltype={WFC;NUC}, unsigned char* Image )
-    #   int ComputeTF (float FrameRate)
-    # + void SetTrueFrames (int Board, int TrueFrames)
-    #
-    # ==== Undocumented ====
-    # + GetInternalTemp
-    #   GetTIFFInfo
-    # + GetCurSeqImage
-    #   GetImageSize
-    #
-    #==== Notes ====
-    # The BNS documentation states that int Board is a 1-based index, but it would appear to be 0-based:
-    # if I address board 1 with Board=1, I get an msc error; using Board=0 seems to work just fine.
+    """ Enables calls to functions in BNS's Interface.dll.
+
+    === BNS Interface.dll functions ===
+    + indicates equivalent python implementation here
+    o indicates calls from non-equivalent python code here
+    
+    ==== Documented ====
+    + int Constructor (int LCType={0:FLC;1:Nematic})
+    + void Deconstructor ()
+    + void ReadTIFF (const char* FilePath, unsigned short* ImageData, unsigned int ScaleWidth, unsigned int ScaleHeight) 
+    + void WriteImage (int Board, unsigned short* Image)
+    + void LoadLUTFile (int Board, char* LUTFileName)
+    o void LoadSequence (int Board, unsigned short* Image, int NumberOfImages)
+    + void SetSequencingRate (double FrameRate)
+    + void StartSequence ()
+    + void StopSequence ()
+    + bool GetSLMPower (int Board)
+    + void SLMPower (int Board, bool PowerOn)
+    + void WriteCal (int Board, CAL_TYPE Caltype={WFC;NUC}, unsigned char* Image )
+      int ComputeTF (float FrameRate)
+    + void SetTrueFrames (int Board, int TrueFrames)
+    
+    ==== Undocumented ====
+    + GetInternalTemp
+      GetTIFFInfo
+    + GetCurSeqImage
+      GetImageSize
+    
+    ==== Notes ====
+    The BNS documentation states that int Board is a 1-based index, but it would appear to be 0-based:
+    if I address board 1 with Board=1, I get an msc error; using Board=0 seems to work just fine.
+    """
     
 
     def __init__(self):
@@ -82,7 +100,7 @@ class BNSDevice(object):
     # === METHODS === #
 
     # Don't call this unless an SLM was initialised:  if you do, the next call can
-    # open that damned dialog box from some other library down the chain.
+    # open a dialog box from some other library down the chain.
     @requires_slm
     def cleanup(self): #tested
         try:
