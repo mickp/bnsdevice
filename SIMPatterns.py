f13c93b82072158fa851fc7f4ff6c65efa0727e1
map <mick.phillips@gmail.com>
2014-06-06 17:32:01 +0100

    Tabs to spaces.

--- SIMPatterns.py
+++ SIMPatterns.py
@@ -3,65 +3,65 @@ from numpy import arange, array, meshgrid, cos, sin, deg2rad, pi
 import matplotlib.pyplot as plt
 
 class SIMPattern3D():
-	def __init__(self):
-		self.angles = array([0,60,120])
-		self.phases = [0, 72, 144, 216, 288]
-		self.pixelPitch = 15e-6
-		self.pixels = 512
-		self.wavelength = 532
-		self.theta = 1
-		self.phaseThenAngle = True
-		
-		self.patterns = []
-		for i in range(0, len(self.angles) * len(self.phases)):
-			self.patterns.append(numpy.zeros((self.pixels, self.pixels)))
-			
-			
-	def updatePatterns(self, angleOffset=None, wavelength=None, theta=None, phaseThenAngle=None):
-		
-		# The pattern-generating function --- defined here so it may 
-		# be used in either phase- or angle-first loops, later.
-		def patternfunc(k, l, thetaRad, mp2):
-			return numpy.round( mp2 * 2 / (2 * pi) * (127.5 + 127.5 * cos ((2 * pi * ( cos(thetaRad) * k + sin(thetaRad) * l)) / pp + deg2rad(phase))))
-		
-		# Pattern ordering, angles and phases.
-		phaseThenAngle = phaseThenAngle or self.phaseThenAngle
-		phases = self.phases
-		if angleOffset is not None:
-			angles = self.angles + angleOffset
-		else:
-			angles = self.angles
-		
-		# Optical parameters - desired diffraction angle and incident wavelength.
-		theta = theta or self.theta
-		wavelength = wavelength or self.wavelength
-		
-		
-		# Pixels per period, from diffraction equation grating_pitch * sin(theta) = order * wavelength
-		# Make sure wavelength is in nm.
-		if wavelength > 100:
-			wavelength *= 1e-9		
-		pp = wavelength / (self.pixelPitch * sin(deg2rad(theta)))
-		
-		# Scaling factor to balance m=0,+/-1 orders
-		mp2 = 1.5
-		
-		# Iterator over self.patterns
-		i = 0
-		
-		indices = arange(self.pixels)
-		k,l = meshgrid(indices,indices)
-		
-		if phaseThenAngle:
-			for angle in angles:
-				for phase in phases:
-					self.patterns[i] = patternfunc(k, l, deg2rad(angle), mp2)
-					i += 1		
-		else:
-			for phase in phases:
-				for angle in angles:
-					self.patterns[i] = patternfunc(k, l, deg2rad(angle), mp2)
-					i += 1		
-			
-	
-	
+    def __init__(self):
+        self.angles = array([0,60,120])
+        self.phases = [0, 72, 144, 216, 288]
+        self.pixelPitch = 15e-6
+        self.pixels = 512
+        self.wavelength = 532
+        self.theta = 1
+        self.phaseThenAngle = True
+        
+        self.patterns = []
+        for i in range(0, len(self.angles) * len(self.phases)):
+            self.patterns.append(numpy.zeros((self.pixels, self.pixels)))
+            
+            
+    def updatePatterns(self, angleOffset=None, wavelength=None, theta=None, phaseThenAngle=None):
+        
+        # The pattern-generating function --- defined here so it may 
+        # be used in either phase- or angle-first loops, later.
+        def patternfunc(k, l, thetaRad, mp2):
+            return numpy.round( mp2 * 2 / (2 * pi) * (127.5 + 127.5 * cos ((2 * pi * ( cos(thetaRad) * k + sin(thetaRad) * l)) / pp + deg2rad(phase))))
+        
+        # Pattern ordering, angles and phases.
+        phaseThenAngle = phaseThenAngle or self.phaseThenAngle
+        phases = self.phases
+        if angleOffset is not None:
+            angles = self.angles + angleOffset
+        else:
+            angles = self.angles
+        
+        # Optical parameters - desired diffraction angle and incident wavelength.
+        theta = theta or self.theta
+        wavelength = wavelength or self.wavelength
+        
+        
+        # Pixels per period, from diffraction equation grating_pitch * sin(theta) = order * wavelength
+        # Make sure wavelength is in nm.
+        if wavelength > 100:
+            wavelength *= 1e-9      
+        pp = wavelength / (self.pixelPitch * sin(deg2rad(theta)))
+        
+        # Scaling factor to balance m=0,+/-1 orders
+        mp2 = 1.5
+        
+        # Iterator over self.patterns
+        i = 0
+        
+        indices = arange(self.pixels)
+        k,l = meshgrid(indices,indices)
+        
+        if phaseThenAngle:
+            for angle in angles:
+                for phase in phases:
+                    self.patterns[i] = patternfunc(k, l, deg2rad(angle), mp2)
+                    i += 1      
+        else:
+            for phase in phases:
+                for angle in angles:
+                    self.patterns[i] = patternfunc(k, l, deg2rad(angle), mp2)
+                    i += 1      
+            
+    
+    
--- slm.py
+++ slm.py
@@ -4,122 +4,122 @@ import os, re, numpy
 from PIL import Image
 
 def loop_in_order(arrayList, order=None):
-	"""Iterate over a list of lists in a specified order."""
-	if order == None:
-		order = range (len (arrayList))
+    """Iterate over a list of lists in a specified order."""
+    if order == None:
+        order = range (len (arrayList))
 
-	order.reverse()
-	pList = product(*[arrayList[n] for n in order])
+    order.reverse()
+    pList = product(*[arrayList[n] for n in order])
 
-	reorder = [order.index(n) for n in range(len(order))]
+    reorder = [order.index(n) for n in range(len(order))]
 
-	for p in pList:
-		yield tuple(p[n] for n in reorder)
+    for p in pList:
+        yield tuple(p[n] for n in reorder)
 
 
 class StripeSeries(list):
-	def __init__(self, pitches=[], phases=[], thetas=[], order=[2,0,1]):
-		super(StripeSeries, self).__init__()
-		self._pitches = pitches
-		self._phases = phases
-		self._thetas = thetas
-		self.extend(list(loop_in_order ([pitches, phases, thetas], order)))
+    def __init__(self, pitches=[], phases=[], thetas=[], order=[2,0,1]):
+        super(StripeSeries, self).__init__()
+        self._pitches = pitches
+        self._phases = phases
+        self._thetas = thetas
+        self.extend(list(loop_in_order ([pitches, phases, thetas], order)))
 
-	def reorder(self, order=[2,0,1]):
-		self[:] = list(loop_in_order ([self._pitches, self._phases, self._thetas], order))
+    def reorder(self, order=[2,0,1]):
+        self[:] = list(loop_in_order ([self._pitches, self._phases, self._thetas], order))
 
 
 class StripePattern(object):
-	def __init__(self, pitch=1, angle=0, phase=0):
-		pass
+    def __init__(self, pitch=1, angle=0, phase=0):
+        pass
 
 
 class SLM(object):
-	def __init__(self):
-		self.LUTs = {}
-		self.calibs = {}
+    def __init__(self):
+        self.LUTs = {}
+        self.calibs = {}
 
-		self._LUTFolder = "LUT_files"
-		self._calibrationFolder = "Phase_Calibration_Files"
-		self.load_calibration_data()
+        self._LUTFolder = "LUT_files"
+        self._calibrationFolder = "Phase_Calibration_Files"
+        self.load_calibration_data()
 
-		self.hardware = BNSDevice()
-		self.pixelPitch = 15e-6
-		self.pixels = (512, 512)
-		self.hardware.initialize()
-
-
-	def generate_stripe_series(self, pitches, phases, thetas, order):
-		pass
+        self.hardware = BNSDevice()
+        self.pixelPitch = 15e-6
+        self.pixels = (512, 512)
+        self.hardware.initialize()
 
 
-	def load_calibration_data(self):
-		""" Loads any calibration data found below module path. """
-		
-		modpath = os.path.dirname(__file__)
-		pattern = '(?P<slm>slm)(?P<serial>[0-9]{2,4})_(?P<wavelength>[0-9]{2,4})'
-
-		path = os.path.join(modpath, self._calibrationFolder)
-		if os.path.exists(path):
-			# load calibration files
-			files = os.listdir(path)			
-			for f in files:
-				try:
-					im = Image.open(os.path.join(path, f))
-					if im.size == self.pixels:
-						calib = {}
-						calib['filename'] = f
-						calib['name'] = os.path.splitext(f)[0]
-						calib['data'] = numpy.array(im)
-
-						match = re.match(pattern, f)
-						if match:
-							calib['wavelength'] = match.group('wavelength')
-							self.calibs[calib['wavelength']] = calib
-						else:
-							calib['wavelength'] = 0
-							self.calibs[calib['name'].lower()] = calib
-
-				except IOError:
-					# couldn't open the image file
-					pass
-				except:
-					raise
-
-		path = os.path.join(modpath, self._LUTFolder)
-		if os.path.exists(path):
-			files = os.listdir(path)			
-			for f in files:
-				try:
-					lut = {}
-					lut['filename'] = f
-					lut['name'] = os.path.splitext(f)[0]
-					lut['data'] = numpy.loadtxt(os.path.join(path, f))
-	
-					match = re.match(pattern, f)
-					if match:
-						lut['wavelength'] = match.group('wavelength')
-						self.LUTs[lut['wavelength']] = lut
-					else:
-						lut['wavelength'] = 0
-						self.LUTs[lut['name'].lower()] = lut
-				except IOError:
-					# couldn't open the file
-					pass
-				except:
-					raise
+    def generate_stripe_series(self, pitches, phases, thetas, order):
+        pass
 
 
-	def load_images(self):
-		pass
+    def load_calibration_data(self):
+        """ Loads any calibration data found below module path. """
+        
+        modpath = os.path.dirname(__file__)
+        pattern = '(?P<slm>slm)(?P<serial>[0-9]{2,4})_(?P<wavelength>[0-9]{2,4})'
+
+        path = os.path.join(modpath, self._calibrationFolder)
+        if os.path.exists(path):
+            # load calibration files
+            files = os.listdir(path)            
+            for f in files:
+                try:
+                    im = Image.open(os.path.join(path, f))
+                    if im.size == self.pixels:
+                        calib = {}
+                        calib['filename'] = f
+                        calib['name'] = os.path.splitext(f)[0]
+                        calib['data'] = numpy.array(im)
+
+                        match = re.match(pattern, f)
+                        if match:
+                            calib['wavelength'] = match.group('wavelength')
+                            self.calibs[calib['wavelength']] = calib
+                        else:
+                            calib['wavelength'] = 0
+                            self.calibs[calib['name'].lower()] = calib
+
+                except IOError:
+                    # couldn't open the image file
+                    pass
+                except:
+                    raise
+
+        path = os.path.join(modpath, self._LUTFolder)
+        if os.path.exists(path):
+            files = os.listdir(path)            
+            for f in files:
+                try:
+                    lut = {}
+                    lut['filename'] = f
+                    lut['name'] = os.path.splitext(f)[0]
+                    lut['data'] = numpy.loadtxt(os.path.join(path, f))
+    
+                    match = re.match(pattern, f)
+                    if match:
+                        lut['wavelength'] = match.group('wavelength')
+                        self.LUTs[lut['wavelength']] = lut
+                    else:
+                        lut['wavelength'] = 0
+                        self.LUTs[lut['name'].lower()] = lut
+                except IOError:
+                    # couldn't open the file
+                    pass
+                except:
+                    raise
 
 
-	def run(self):
-		self.hardware.power = True
-		self.hardware.start_sequence()
+    def load_images(self):
+        pass
 
 
-	def stop(self):
-		self.hardware.stop_sequence()
-		self.hardware.power = False
+    def run(self):
+        self.hardware.power = True
+        self.hardware.start_sequence()
+
+
+    def stop(self):
+        self.hardware.stop_sequence()
+        self.hardware.power = False
 
