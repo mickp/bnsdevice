8046eeeaac8d77911b45c3f43ecfbbc9df7e1db7
Mick Phillips <mick.phillips@gmail.com>
2014-06-08 15:11:02 +0100

    More style improvements.

--- slm.py
+++ slm.py
@@ -1,3 +1,5 @@
+""" Wrapper around BNS Interface.dll """
+
 from bnsdummy import BNSDevice
 from itertools import chain, product
 import os, re, numpy
@@ -6,7 +8,7 @@ from PIL import Image
 def loop_in_order(arrayList, order=None):
     """Iterate over a list of lists in a specified order."""
     if order == None:
-        order = range (len (arrayList))
+        order = range(len(arrayList))
 
     order.reverse()
     pList = product(*[arrayList[n] for n in order])
@@ -18,15 +20,17 @@ def loop_in_order(arrayList, order=None):
 
 
 class StripeSeries(list):
-    def __init__(self, pitches=[], phases=[], thetas=[], order=[2,0,1]):
+    def __init__(self, pitches=[], phases=[], thetas=[], order=[2, 0, 1]):
         super(StripeSeries, self).__init__()
         self._pitches = pitches
         self._phases = phases
         self._thetas = thetas
-        self.extend(list(loop_in_order ([pitches, phases, thetas], order)))
+        self.extend(list(loop_in_order([pitches, phases, thetas], order)))
 
-    def reorder(self, order=[2,0,1]):
-        self[:] = list(loop_in_order ([self._pitches, self._phases, self._thetas], order))
+    def reorder(self, order=[2, 0, 1]):
+        self[:] = list(loop_in_order(
+                    [self._pitches, self._phases, self._thetas],
+                    order))
 
 
 class StripePattern(object):
@@ -55,14 +59,14 @@ class SLM(object):
 
     def load_calibration_data(self):
         """ Loads any calibration data found below module path. """
-        
+
         modpath = os.path.dirname(__file__)
         pattern = '(?P<slm>slm)(?P<serial>[0-9]{2,4})_(?P<wavelength>[0-9]{2,4})'
 
         path = os.path.join(modpath, self._calibrationFolder)
         if os.path.exists(path):
             # load calibration files
-            files = os.listdir(path)            
+            files = os.listdir(path)
             for f in files:
                 try:
                     im = Image.open(os.path.join(path, f))
@@ -88,14 +92,14 @@ class SLM(object):
 
         path = os.path.join(modpath, self._LUTFolder)
         if os.path.exists(path):
-            files = os.listdir(path)            
+            files = os.listdir(path)
             for f in files:
                 try:
                     lut = {}
                     lut['filename'] = f
                     lut['name'] = os.path.splitext(f)[0]
                     lut['data'] = numpy.loadtxt(os.path.join(path, f))
-    
+
                     match = re.match(pattern, f)
                     if match:
                         lut['wavelength'] = match.group('wavelength')
