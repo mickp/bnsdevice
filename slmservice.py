e29a68a16efa5041c49af3ad6fd8d594964f0419
Mick Phillips <mick.phillips@gmail.com>
2015-03-17 14:49:31 +0000

    Cleaned up SLM init, removed old pattern functions.

--- slmservice.py
+++ slmservice.py
@@ -38,66 +38,32 @@ logging.basicConfig(level=logging.INFO,
 
 class SpatialLightModulator(object):
     def __init__(self):
+        # Logging
         loggerName = '.'.join([__name__, self.__class__.__name__])
         self.logger = logging.getLogger(loggerName)
-        
-        self.LUTs = {}
-        self.calibs = {}
-        self.pixel_pitch = 15.0 # in microns
+        ## SLM geometry
+        # Physical pitch in microns
+        self.pixel_pitch = 15.0
+        # SLM size in pixels
         self.pixels = (512, 512)
-
+        # Might as well evaluate image indices only once
+        x_range = arange(self.pixels[0])
+        y_range = arange(self.pixels[1])
+        self.kk, self.ll = meshgrid(x_range, y_range)
+        ## Image sequence
+        self.sequence = []
+        ## Look-up tables and calibration data
+        # Paths
         self._LUTFolder = "LUT_files"
         self._calibrationFolder = "Phase_Calibration_Files"
+        # Mapped by wavelength
+        self.luts = {}
+        self.calibs = {}
+        # Load calib. data and LUT files
         self.load_calibration_data()
-
+        ## Connect to the hardware.
         self.hardware = BNSDevice()     
         self.hardware.initialize()
-        #self.hardware.load_lut('linear.lut')
-
-        self.sequence = []
-
-
-    def generate_test_sequence(self, low, high):
-        low = min(low * (low > 0), high * (high > 0))
-        high = max(65535, low * (low > 0), high * (high > 0))
-
-        self.sequence = []
-        xindices = arange(self.pixels[0])
-        yindices = arange(self.pixels[1])
-        kk, ll = meshgrid(xindices, yindices)
-        for i in range(3):
-            pattern = numpy.ushort(
-                low + (high - low) * 
-                (kk % (self.pixels[0] / (i+1)) / (self.pixels[0] - 1))
-                )
-            self.sequence.append(pattern)
-        for j in range(3):
-            pattern = numpy.ushort(
-                ll % (self.pixels[1] / (j+1))
-                )
-            self.sequence.append(pattern)
-        return len(self.sequence)
-
-
-
-    def generate_stripe_series(self, patternparms):
-        """ Generate a stripe series from the patternparms.
-
-        patternparms is a list of tuples:  (pitch, angle, phase, wavelength),
-        where pitch is in microns, and both the angle and phase are specified
-        in radians.  
-        """
-        self.sequence = []
-        xindices = arange(self.pixels[0])
-        yindices = arange(self.pixels[1])
-        kk, ll = meshgrid(xindices, yindices)
-        for realpitch, angle, phase, wavelength in patternparms:
-            pitch = realpitch / self.pixel_pitch
-            pattern = numpy.ushort(
-                    rint(32767.5 + 32767.5 * cos(phase +
-                    2 * pi * (cos(angle) * kk + sin(angle) * ll) / pitch)))
-            self.sequence.append(pattern)
-        return len(self.sequence)
 
 
     def load_calibration_data(self):
@@ -175,7 +141,7 @@ class SpatialLightModulator(object):
 
             wavelength = match.groupdict()['wavelength']
             lut['wavelength'] = wavelength
-            self.LUTs[wavelength] = lut
+            self.luts[wavelength] = lut
             self.logger.info("\tloaded data from %s" % f)
 
         return None
